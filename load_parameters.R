d0286da1e309416dd4cfbbaaf30dbd73e8244d72
lindsaywaldrop <waldrop@chapman.edu>
2019-11-17 13:05:53 -0800

    Adding a more realistic wing shape (panelwing) analysis, separate from original

--- load_parameters.R
+++ load_parameters.R
@@ -14,6 +14,8 @@ names(inputs1233)<-c("AR","Camber","Re") # Sets names to columns
 # Create factor data class columns for each parameter, record levels
 inputs681$ARFac<-as.factor(inputs681$AR) 
 inputs1233$ARFac<-as.factor(inputs1233$AR) 
+inputs681$speeds<-calcSpeeds(inputs681$Re,0.1)
+inputs1233$speeds<-calcSpeeds(inputs1233$Re,0.1)
 levels618.AR<-levels(inputs681$ARFac)
 levels1233.AR<-levels(inputs1233$ARFac)
 
@@ -65,16 +67,53 @@ calcAR(as.numeric(as.character(levels.AR[1])),0.1)
 
 calcSpeeds(as.numeric(as.character(levels.Re[1])),0.1)
 
-runs<-parameters[parameters$Camber==levels.Camber[2],]
+runs<-parameters[parameters$Camber==levels.Camber[9],]
 nrow(parameters[parameters$Camber==levels.Camber[23],])
 
 progress<-0
-for(i in 1:21){
+for(i in 1:8){
 progress<-progress+nrow(parameters[parameters$Camber==levels.Camber[i],])
 }
 progress/681
   
 calcAR(10.098076,0.1)
+
+#### Wing area calculation for diamond wings ####
+
+# Aspect ratio = (2*wing length)^2 / wing area
+
+rm(list=ls())
+
+C<-c(0.059,0.140,0.139,0.129,0.099,0.034)
+offsets<-c(0,-0.03,-0.04,-0.03,-0.01,0.03)
+L<-seq(0,2,by=0.001)
+
+wingarea<-function(L,C,offsets){
+  Lm<-L/5
+  a1<-rep(NA,length(C)-1)
+  a2<-rep(NA,length(C)-1)
+  a3<-rep(NA,length(C)-1)
+  for(i in 1:length(C)-1){
+    a1[i]<-C[i]*Lm
+    a2[i]<-0.5*Lm*(C[i+1]-C[i]+(offsets[i+1]-offsets[i]))
+    a3[i]<-0.5*Lm*(-offsets[i+1]+offsets[i])
+  }
+  return(2*(sum(a1)+sum(a2)+sum(a3)))
+}
+
+AR<-rep(NA,length(L))
+for (k in 1:length(L)){
+  AR[k]<-(2*L[k])^2/wingarea(L[k],C,offsets)
+}
+plot(L,AR)
+
+model<-lm(AR~L)
+slope<-as.numeric(model[["coefficients"]][2])
+
+findL<-function(AR){AR/18.06685}
+abline(model)
+
+2*L/(mean(C)*L)
 
 
 
